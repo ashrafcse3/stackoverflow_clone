de194e2fd8d7be3a8b64e49aefeab6bc91f6ec8b
Ashraful alam <ashrafcse3@gmail.com>
2019-01-28 23:02:19 +0600

    add latest sql file

--- login.sql
+++ login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2019 at 05:40 PM
+-- Generation Time: Jan 28, 2019 at 06:01 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -25,6 +25,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(15) NOT NULL,
+  `cat_title` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `cat_title`) VALUES
+(1, 'Web Developing'),
+(2, 'Artificial Intelligence'),
+(3, 'Language Learning'),
+(4, 'Travel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(15) NOT NULL,
+  `user_id` int(15) NOT NULL,
+  `cat_id` int(15) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `cat_id`, `title`, `description`) VALUES
+(1, 27, 1, 'Why do we use it?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
+(2, 31, 2, 'Where can I get some?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
+(3, 32, 3, 'Where does it come from?', 'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskt'),
+(4, 28, 4, 'What is Lorem Ipsam?', 'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskt');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -32,21 +77,39 @@ CREATE TABLE `users` (
   `id` int(15) NOT NULL,
   `name` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(256) NOT NULL,
+  `hash` varchar(100) NOT NULL,
+  `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Ashraful Alam', 'ashrafcse3@gmail.com', 'sos'),
-(3, 'Tufayel Alam', 'noid@gmail.com', 'sos'),
-(4, 'Afzal', 'afzal@gmail.com', 'sos');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `hash`, `active`) VALUES
+(27, 'Ashraful Alam', 'ashrafcse3@gmail.com', '$2y$10$SqQbpsABRGf/zLQnbbOSHuKl2dy3ToAuDkt4KeVmfjzNR.R3Mggyu', 'c9d9edbf9b9e23eb5d4819bbcce9b078', 1),
+(28, 'AA', 'noid@gmail.com', '$2y$10$hLqKkXiQJ3njMELubj48D.SiED4SrzjI/yHXackPPJijTMCRAeSP2', 'bd2319431f3e50192180e12334eb52da', 1),
+(29, 'Ashraful Alam', 'saleh@gmail.com', '$2y$10$c/TC.rPEwOJSdCZJUC.LlOeO8jMRdv/rxoMwfvLD3sbpyv92uYZ8a', '654a15a7b10edf676cbd37e00815ae89', 1),
+(30, 'AA', 'user@user.com', '$2y$10$GwXTYBnkcDMl4JN6SuyVpOUYQ4AJvyhR0VY/maGBgXHC5EPAKJeIO', '2b0fd97cf4bbb9a800b9004e5961bb0d', 0),
+(31, 'Adnan', 'adnan@gmail.com', '$2y$10$z57eCijYtBp5qXGHtseV7uIN0r.dbcDZdu9APTR/QAP6kjZqmQyjW', '373eb24e9055ded5b02698f7c7f7d4b8', 1),
+(32, 'nazmul', 'Nazmul@gmail.com', '$2y$10$yQJMw2JYgSt2aZnyWPcx8.Kp9Ta/HTc6hoOp/BR8FLswHDQM8tA.e', '8969a7f7fe8b1fbc83fe4252322bd640', 1),
+(33, 'Shuvo', 'shuvo@gmail.com', '$2y$10$Pxr4ca2QU0xT8aXWBz07Hu6vFzjhEsSonjkTXWV4NpZ.fr6in0bU.', '7125a4273f9840b9cebb0a440265fe77', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -59,10 +122,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
